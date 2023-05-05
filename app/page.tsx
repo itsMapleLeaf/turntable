7e9c285088b5bc022add384857b3cfdddd75bf97
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-05-04 23:28:55 -0500

    submit via server action

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,14 @@
 "use client"
-import { useMutation } from "@tanstack/react-query"
 import { Disc } from "lucide-react"
-import { useState } from "react"
+import { useState, useTransition } from "react"
+import { submitSong } from "./actions"
 
 const streamUrl = new URL(
   "/v1/audio/stream",
   process.env.NEXT_PUBLIC_SERVER_URL,
 )
-const submitUrl = new URL("/v1/audio/input", process.env.NEXT_PUBLIC_SERVER_URL)
 
 export default function Home() {
-  const [songUrl, setSongUrl] = useState("")
-
-  const submitSong = useMutation({
-    mutationFn: async () => {
-      const response = await fetch(submitUrl, { method: "POST", body: songUrl })
-      if (!response.ok) {
-        throw new Error(
-          `Failed to submit song (${response.status} ${response.statusText})`,
-        )
-      }
-    },
-    onSuccess() {
-      setSongUrl("")
-    },
-  })
-
   return (
     <>
       <main className="flex flex-1 flex-col gap-4 p-8">
@@ -36,27 +19,7 @@ export default function Home() {
 
         <section className="max-w-sm bg-black/75 w-full p-4 mx-auto">
           <h2>Add Song</h2>
-          <form
-            onSubmit={(event) => {
-              event.preventDefault()
-              submitSong.mutate()
-            }}
-          >
-            <input
-              placeholder="Stream URL"
-              value={songUrl}
-              onChange={(event) => setSongUrl(event.target.value)}
-              className="bg-black/50"
-            />
-            <button>{submitSong.isLoading ? "Submitting..." : "Submit"}</button>
-            {submitSong.error ? (
-              <p className="text-red-400">
-                {submitSong.error instanceof Error
-                  ? submitSong.error.message
-                  : "Could not submit song"}
-              </p>
-            ) : null}
-          </form>
+          <AddSongForm />
         </section>
 
         <section className="max-w-sm bg-black/75 w-full p-4 mx-auto flex-1">
@@ -67,5 +30,31 @@ export default function Home() {
         <button onClick={() => new Audio(streamUrl.href).play()}>Play</button>
       </footer>
     </>
+  )
+}
+
+function AddSongForm() {
+  const [songUrl, setSongUrl] = useState("")
+  const [pending, startTransition] = useTransition()
+  const [error, setError] = useState<string>()
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault()
+        startTransition(async () => {
+          const result = await submitSong(songUrl)
+          if (result.error) setError(result.error)
+        })
+      }}
+    >
+      <input
+        placeholder="Stream URL"
+        value={songUrl}
+        onChange={(event) => setSongUrl(event.target.value)}
+        className="bg-black/50"
+      />
+      <button>{pending ? "Submitting..." : "Submit"}</button>
+      {error ? <p className="text-red-400">{error}</p> : null}
+    </form>
   )
 }
