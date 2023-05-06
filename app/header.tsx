f9a2960718732952a02b5833907d946e5db1c2cc
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-05-05 22:15:03 -0500

    show username in header

--- app/header.tsx
+++ app/header.tsx
@@ -1,8 +1,12 @@
 import { Disc, UserPlus } from "lucide-react"
+import { cookies } from "next/headers"
 import Link from "next/link"
 import { HeaderMenu } from "./header-menu"
+import { vinylApi } from "./vinyl-api"
 
-export function Header() {
+export async function Header() {
+  const user = await vinylApi(cookies().get("vinyl_token")?.value).getUser()
+
   return (
     <header className="h-16 flex flex-row items-center panel border-b sticky top-0 z-10">
       <nav className="container flex flex-row items-center">
@@ -16,9 +20,17 @@ export function Header() {
         </Link>
 
         <div className="flex-1 flex flex-row justify-end">
-          <Link href="/sign-up" className="button">
-            <UserPlus className="w-5 h-5" aria-hidden /> Sign up
-          </Link>
+          {user.data ? (
+            <p className="leading-tight text-right">
+              <span className="text-sm opacity-75">Signed in as</span>
+              <br />
+              {user.data.display_name}
+            </p>
+          ) : (
+            <Link href="/sign-up" className="button">
+              <UserPlus className="w-5 h-5" aria-hidden /> Sign up
+            </Link>
+          )}
         </div>
       </nav>
     </header>
