2dac7204e8982ca2b0e7baa33629cee60b9da09b
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-05-07 14:31:59 -0500

    proper empty state for room list

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -12,33 +12,51 @@ export async function loader({ request }: LoaderArgs) {
 }
 
 export default function RoomListPage() {
-  const { rooms } = useLoaderData<typeof loader>()
   return (
     <main className="flex-1 flex-col p-4 container">
-      {"data" in rooms ? (
-        <ul className="gap-4 grid grid-cols-[repeat(auto-fill,minmax(12rem,1fr))]">
-          {rooms.data.map((room) => (
-            <li key={room.id}>
-              <Link
-                to={`/rooms/${room.id}`}
-                className="panel panel-interactive p-4 flex flex-row items-center gap-4 border"
-              >
-                <Disc size={32} aria-hidden />
-                <span className="text-lg/5">
-                  {room.name}
-                  <br />
-                  <span className="text-sm opacity-75">
-                    {room.connections.length} listener
-                    {room.connections.length === 1 ? "" : "s"}
-                  </span>
-                </span>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p className="text-center">{rooms.error}</p>
-      )}
+      <RoomListPageContent />
     </main>
+  )
+}
+
+function RoomListPageContent() {
+  const { rooms } = useLoaderData<typeof loader>()
+
+  if ("error" in rooms) {
+    return <p className="text-center">{rooms.error}</p>
+  }
+
+  if (!rooms.data.length) {
+    return (
+      <div className="flex flex-col items-center gap-4 text-center py-16">
+        <p className="text-2xl font-light">No rooms found.</p>
+        <Link to="/rooms/new" className="button">
+          Create one!
+        </Link>
+      </div>
+    )
+  }
+
+  return (
+    <ul className="gap-4 grid grid-cols-[repeat(auto-fill,minmax(12rem,1fr))]">
+      {rooms.data.map((room) => (
+        <li key={room.id}>
+          <Link
+            to={`/rooms/${room.id}`}
+            className="panel panel-interactive p-4 flex flex-row items-center gap-4 border"
+          >
+            <Disc size={32} aria-hidden />
+            <span className="text-lg/5">
+              {room.name}
+              <br />
+              <span className="text-sm opacity-75">
+                {room.connections.length} listener
+                {room.connections.length === 1 ? "" : "s"}
+              </span>
+            </span>
+          </Link>
+        </li>
+      ))}
+    </ul>
   )
 }
