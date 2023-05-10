679cb853e9eec97d8b04b5b0750f826109cd2b77
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-05-10 01:21:56 -0500

    async iterators are fun

--- app/vinyl/vinyl-socket.ts
+++ app/vinyl/vinyl-socket.ts
@@ -1,4 +1,6 @@
 import { z } from "zod"
+import { delay } from "~/helpers/delay"
+import { socket } from "~/helpers/socket"
 import { trackSchema } from "./vinyl-types"
 
 const socketMessageSchema = z.union([
@@ -39,44 +41,44 @@ export function vinylSocket({
   url: string
   onMessage: (message: SocketMessage) => void
 }) {
-  let socket: WebSocket | undefined
+  const controller = new AbortController()
+  let running = true
 
-  function connect() {
-    socket = new WebSocket(url)
-    socket.addEventListener("message", handleMessage)
-    socket.addEventListener("close", handleClose)
-    socket.addEventListener("error", handleError)
-  }
+  async function run() {
+    while (running) {
+      try {
+        const connection = await socket(url, { signal: controller.signal })
+        console.info("Connected to socket")
 
-  function handleMessage(event: MessageEvent) {
-    try {
-      const result = socketMessageSchema.safeParse(
-        JSON.parse(event.data as string),
-      )
-      if (result.success) {
-        onMessage(result.data)
-      } else {
-        console.error("Unknown socket message:", result.error)
+        for await (const event of connection) {
+          try {
+            const result = socketMessageSchema.safeParse(JSON.parse(event.data))
+            if (result.success) {
+              onMessage(result.data)
+            } else {
+              console.error("Unknown socket message:", result.error)
+            }
+          } catch (error) {
+            console.error("Failed to parse socket message:", error)
+          }
+        }
+
+        console.info("Socket closed")
+      } catch (error) {
+        console.error("Failed to connect to socket:", error)
       }
-    } catch (error) {
-      console.error("Failed to parse socket message:", error)
+
+      if (running) {
+        console.info("Reconnecting...")
+        await delay(2000)
+      }
     }
   }
 
-  function handleClose() {
-    setTimeout(connect, 1000)
-  }
-
-  function handleError() {
-    setTimeout(connect, 1000)
-  }
-
-  connect()
+  void run()
 
   return () => {
-    socket?.removeEventListener("message", handleMessage)
-    socket?.removeEventListener("close", handleClose)
-    socket?.removeEventListener("error", handleError)
-    socket?.close()
+    running = false
+    controller.abort()
   }
 }
