bc6a2560f63cc28e06bae5d41906292253eda3d0
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-05-11 12:05:15 -0500

    do things for firefox

--- app/routes/rooms.$roomId/route.tsx
+++ app/routes/rooms.$roomId/route.tsx
@@ -70,7 +70,7 @@ function RoomPageContent({ room }: { room: Room }) {
   const playing = useStreamPlaying()
 
   const play = useCallback(() => {
-    playStream(`/rooms/${room.id}/stream`)
+    playStream(`/rooms/${room.id}/stream.wav?nocache=${Date.now()}`)
   }, [room.id])
 
   useEffect(() => {
