7c0c95ff454c55f8f506d7bf09cd187151b3f386
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-05-14 13:51:52 -0500

    show artist/artwork in now playing

--- app/routes/rooms.$roomId/now-playing.tsx
+++ app/routes/rooms.$roomId/now-playing.tsx
@@ -8,23 +8,33 @@ export function NowPlaying() {
   const item = useCurrentRoomQueueItem()
   const progress = useRoomSongProgress()
   return (
-    <div className="flex flex-1 flex-col text-center leading-5 sm:text-right">
+    <div className="flex flex-1 flex-col justify-center text-center leading-5 gap-2 sm:flex-row items-center sm:text-right sm:justify-end">
       {item ? (
         <>
-          <p className="text-sm opacity-75 tabular-nums">
-            {prettyMs(progress * 1000, {
-              colonNotation: true,
-              secondsDecimalDigits: 0,
-            })}{" "}
-            /{" "}
-            {prettyMs(item.track.metadata.duration * 1000, {
-              colonNotation: true,
-              secondsDecimalDigits: 0,
-            })}
-          </p>
-          <p className="line-clamp-1 [word-break:break-all]">
-            {item.track.metadata.title}
-          </p>
+          <div>
+            <p className="text-sm opacity-75 tabular-nums">
+              {item.track.metadata.artist} &bull;{" "}
+              {prettyMs(progress * 1000, {
+                colonNotation: true,
+                secondsDecimalDigits: 0,
+              })}{" "}
+              /{" "}
+              {prettyMs(item.track.metadata.duration * 1000, {
+                colonNotation: true,
+                secondsDecimalDigits: 0,
+              })}
+            </p>
+            <p className="line-clamp-1 [word-break:break-all]">
+              {item.track.metadata.title}
+            </p>
+          </div>
+          {item.track.metadata.artwork && (
+            <img
+              src={item.track.metadata.artwork}
+              alt=""
+              className="w-12 h-12 ml-2 object-cover"
+            />
+          )}
         </>
       ) : (
         <p className="opacity-75">Nothing playing</p>
