7ad59a0f5481e1e057a21cb156414704220eae8f
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-05-15 08:52:02 -0500

    hide played items again

--- app/routes/rooms.$roomId/room-queue.tsx
+++ app/routes/rooms.$roomId/room-queue.tsx
@@ -4,35 +4,41 @@ import { useCurrentRoomQueueItem, useRoomQueue } from "./room-state-context"
 export function RoomQueue() {
   const queue = useRoomQueue()
   const current = useCurrentRoomQueueItem()
+  const currentIndex = queue.items.findIndex((i) => i.id === current?.id)
+
+  const getSubmitterName = (submitterId: string) =>
+    queue.submitters.find((s) => s.id === submitterId)?.display_name ??
+    "unknown"
+
   return (
     <ul className="panel isolate border">
-      {queue.items.map(({ id, submitter, track: { metadata } }) => (
-        <li
-          key={id}
-          data-current={current?.id === id || undefined}
-          className="-m-px flex flex-row gap-3 border border-transparent from-accent-200/10 p-3 data-[current]:border-accent-200/25 data-[current]:bg-gradient-to-r data-[current]:text-accent-200"
-        >
-          {metadata.artwork ? (
-            <img
-              src={metadata.artwork}
-              alt=""
-              className="h-12 w-12 rounded border border-white/10 object-cover"
-            />
-          ) : null}
-          <div className="flex flex-1 flex-col justify-center">
-            <p className="text-sm/5 opacity-75">
-              {metadata.artist} &bull;{" "}
-              {prettyMilliseconds(metadata.duration * 1000, {
-                colonNotation: true,
-              })}{" "}
-              &bull; added by{" "}
-              {queue.submitters.find((s) => s.id === submitter)?.display_name ??
-                "unknown"}
-            </p>
-            <p className="text-lg/5">{metadata.title}</p>
-          </div>
-        </li>
-      ))}
+      {queue.items
+        .slice(currentIndex)
+        .map(({ id, submitter, track: { metadata } }) => (
+          <li
+            key={id}
+            data-current={current?.id === id || undefined}
+            className="-m-px flex flex-row gap-3 border border-transparent from-accent-200/10 p-3 data-[current]:border-accent-200/25 data-[current]:bg-gradient-to-r data-[current]:text-accent-200"
+          >
+            {metadata.artwork ? (
+              <img
+                src={metadata.artwork}
+                alt=""
+                className="h-12 w-12 rounded border border-white/10 object-cover"
+              />
+            ) : null}
+            <div className="flex flex-1 flex-col justify-center">
+              <p className="text-sm/5 opacity-75">
+                {metadata.artist} &bull;{" "}
+                {prettyMilliseconds(metadata.duration * 1000, {
+                  colonNotation: true,
+                })}{" "}
+                &bull; added by {getSubmitterName(submitter)}
+              </p>
+              <p className="text-lg/5">{metadata.title}</p>
+            </div>
+          </li>
+        ))}
     </ul>
   )
 }
