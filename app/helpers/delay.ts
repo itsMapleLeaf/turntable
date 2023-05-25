417a382727d0a5ddc84ae1945ba2d067e7046d4c
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-05-25 16:36:02 -0500

    delay accepts an abort signal

--- app/helpers/delay.ts
+++ app/helpers/delay.ts
@@ -1,2 +1,9 @@
-export const delay = (ms: number) =>
-  new Promise<void>((resolve) => setTimeout(resolve, ms))
+export const delay = (ms: number, options: { signal?: AbortSignal } = {}) =>
+  new Promise<void>((resolve, reject) => {
+    setTimeout(resolve, ms)
+    options?.signal?.addEventListener(
+      "abort",
+      () => reject(new Error("Aborted")),
+      { once: true },
+    )
+  })
