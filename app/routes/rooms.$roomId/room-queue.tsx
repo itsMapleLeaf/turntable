23369a18333cd9427276d6cee9bb9aca6a015d37
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-05-25 16:51:57 -0500

    add canonical link

--- app/routes/rooms.$roomId/room-queue.tsx
+++ app/routes/rooms.$roomId/room-queue.tsx
@@ -11,33 +11,37 @@ export function RoomQueue() {
     "unknown"
 
   return (
-    <ul className="panel isolate border">
+    <ul className="panel border">
       {queue.items
         .slice(currentIndex)
         .map(({ id, submitter, track: { metadata } }) => (
-          <li
-            key={id}
-            data-current={current?.id === id || undefined}
-            className="-m-px flex flex-row gap-3 border border-transparent from-accent-200/10 p-3 data-[current]:border-accent-200/25 data-[current]:bg-gradient-to-r data-[current]:text-accent-200"
-          >
-            {metadata.artwork ? (
-              <img
-                src={metadata.artwork}
-                alt=""
-                className="h-12 w-12 rounded border border-white/10 object-cover"
-              />
-            ) : null}
-            <div className="flex flex-1 flex-col justify-center">
-              <p className="text-sm/5 opacity-75">
-                {metadata.artist} &bull;{" "}
-                {prettyMilliseconds(metadata.duration * 1000, {
-                  colonNotation: true,
-                  secondsDecimalDigits: 0,
-                })}{" "}
-                &bull; added by {getSubmitterName(submitter)}
-              </p>
-              <p className="text-lg/5">{metadata.title}</p>
-            </div>
+          <li key={id} className="-m-px">
+            <a
+              href={metadata.canonical}
+              target="_blank"
+              rel="noopener noreferrer"
+              data-current={current?.id === id || undefined}
+              className="group flex flex-row gap-3 border border-transparent from-accent-200/10 p-3 transition-colors hover:text-accent-200 data-[current]:border-accent-200/25 data-[current]:bg-gradient-to-r data-[current]:text-accent-200"
+            >
+              {metadata.artwork ? (
+                <img
+                  src={metadata.artwork}
+                  alt=""
+                  className="h-12 w-12 rounded border border-white/10 object-cover transition-colors group-hover:border-accent-200/25"
+                />
+              ) : null}
+              <div className="flex flex-1 flex-col justify-center">
+                <p className="text-sm/5 opacity-75">
+                  {metadata.artist} &bull;{" "}
+                  {prettyMilliseconds(metadata.duration * 1000, {
+                    colonNotation: true,
+                    secondsDecimalDigits: 0,
+                  })}{" "}
+                  &bull; added by {getSubmitterName(submitter)}
+                </p>
+                <p className="text-lg/5">{metadata.title}</p>
+              </div>
+            </a>
           </li>
         ))}
     </ul>
