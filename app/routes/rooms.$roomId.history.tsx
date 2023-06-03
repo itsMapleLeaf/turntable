bd94e3dc9a3c14fb84ee6b54324282d8a2a108f3
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-06-03 17:45:26 -0500

    history

--- app/routes/rooms.$roomId.history.tsx
+++ app/routes/rooms.$roomId.history.tsx
@@ -1,12 +1,42 @@
-import { useParams } from "@remix-run/react"
-import { $params } from "remix-routes"
+import {
+  useCurrentRoomQueueItem,
+  useRoomQueue,
+} from "~/components/room-state-context"
+import { SongList, SongListItem } from "~/components/song-list"
 
 export default function RoomHistoryPage() {
-  const { roomId } = $params("/rooms/:roomId/history", useParams())
+  const queue = useRoomQueue()
+  const current = useCurrentRoomQueueItem()
+  const currentIndex = queue.items.findIndex((i) => i.id === current?.id)
+  const historyItems = queue.items.slice(0, currentIndex)
+
+  const getSubmitterName = (submitterId: string) =>
+    queue.submitters.find((s) => s.id === submitterId)?.display_name ??
+    "unknown"
+
   return (
     <section className="grid gap-4">
       <h2 className="sr-only">History</h2>
-      the
+      {historyItems.length > 0 ? (
+        <SongList>
+          {historyItems.map(({ id, submitter, track: { metadata } }) => (
+            <SongListItem
+              key={id}
+              title={metadata.title}
+              artist={metadata.artist}
+              addedBy={getSubmitterName(submitter)}
+              durationSeconds={metadata.duration}
+              artwork={metadata.artwork}
+              isActive={current?.id === id}
+              link={metadata.canonical}
+            />
+          ))}
+        </SongList>
+      ) : (
+        <p className="panel border p-3">
+          <span className="opacity-75">This room has no history.</span>
+        </p>
+      )}
     </section>
   )
 }
