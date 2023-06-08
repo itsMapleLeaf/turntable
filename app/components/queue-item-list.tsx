aad454db5aff3b2d88c74c8facb13af184141f36
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-06-07 21:16:23 -0500

    virtualize song list

--- app/components/queue-item-list.tsx
+++ app/components/queue-item-list.tsx
@@ -1,8 +1,9 @@
+import prettyMilliseconds from "pretty-ms"
+import { Virtuoso } from "react-virtuoso"
 import {
   useCurrentRoomQueueItem,
   useRoomQueue,
 } from "~/components/room-state-context"
-import { SongList, SongListItem } from "~/components/song-list"
 import { type QueueItem } from "~/data/vinyl-types"
 
 export function QueueItemList({ items }: { items: QueueItem[] }) {
@@ -14,20 +15,39 @@ export function QueueItemList({ items }: { items: QueueItem[] }) {
     "unknown"
 
   return (
-    <SongList>
-      {items.map(({ id, submitter, track: { metadata } }) => (
-        <SongListItem
-          key={id}
-          title={metadata.title}
-          artist={metadata.artist}
-          addedBy={getSubmitterName(submitter)}
-          addedFrom={metadata.source}
-          durationSeconds={metadata.duration}
-          artwork={metadata.artwork}
-          isActive={current?.id === id}
-          link={metadata.canonical}
-        />
-      ))}
-    </SongList>
+    <Virtuoso
+      data={items}
+      className="panel border"
+      useWindowScroll
+      itemContent={(_index, { id, submitter, track: { metadata } }) => (
+        <a
+          href={metadata.canonical}
+          target="_blank"
+          rel="noopener noreferrer"
+          data-active={current?.id === id || undefined}
+          className="group flex flex-row gap-3 border border-transparent from-accent-200/10 p-3 transition-colors hover:text-accent-200 data-[active]:border-accent-200/25 data-[active]:bg-gradient-to-r data-[active]:text-accent-200"
+        >
+          {metadata.artwork ? (
+            <img
+              src={metadata.artwork}
+              alt=""
+              className="h-12 w-12 rounded border border-white/10 object-cover transition-colors group-hover:border-accent-200/25"
+            />
+          ) : null}
+          <div className="flex flex-1 flex-col justify-center">
+            <p className="text-sm/5 opacity-75">
+              {metadata.artist} &bull;{" "}
+              {prettyMilliseconds(metadata.duration * 1000, {
+                colonNotation: true,
+                secondsDecimalDigits: 0,
+              })}{" "}
+              &bull; added by {getSubmitterName(submitter)} &bull;{" "}
+              {metadata.source}
+            </p>
+            <p className="text-lg/5">{metadata.title}</p>
+          </div>
+        </a>
+      )}
+    />
   )
 }
