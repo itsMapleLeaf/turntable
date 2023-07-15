fabd818d621279f3fe6c16c9732937d86f29361d
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-07-15 14:55:50 -0500

    use span for spinner
    so react doesn't complain when this is in a p

--- app/components/spinner.tsx
+++ app/components/spinner.tsx
@@ -4,13 +4,13 @@ export function Spinner({ size = 5 }: { size?: 4 | 5 }) {
     5: "h-5 w-5 gap-1",
   }
   return (
-    <div
+    <span
       className={`grid animate-spin grid-cols-2 grid-rows-2 ${sizeClasses[size]}`}
     >
-      <div className="rounded-full bg-accent-200" />
-      <div className="rounded-full bg-accent-200" />
-      <div className="rounded-full bg-accent-200" />
-      <div className="rounded-full bg-accent-200" />
-    </div>
+      <span className="rounded-full bg-accent-200" />
+      <span className="rounded-full bg-accent-200" />
+      <span className="rounded-full bg-accent-200" />
+      <span className="rounded-full bg-accent-200" />
+    </span>
   )
 }
